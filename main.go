bbf6dd176ac37e0b2f5573a7b9c7e7e6a1c13511
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-10-06 12:43:10 +0300

    create data file on start

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/hussein-mourad/gotasks/cmd"
+import (
+	"log"
+	"os"
+
+	"github.com/hussein-mourad/gotasks/cmd"
+)
 
 func main() {
+	createFile()
 	cmd.Execute()
+}
+
+func createFile() {
+	filename := "data.csv"
+	_, err := os.Open(filename)
+	if err != nil {
+		_, err = os.Create(filename)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
